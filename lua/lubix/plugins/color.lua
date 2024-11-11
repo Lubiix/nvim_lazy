59e716cfd72e6b710be2b993769b07aa4c079fc6
Lubiix <maxime.violi@gmail.com>
2024-11-11 14:55:41 +0100

    change colors and add remap

---
+++ lua/lubix/plugins/color.lua
@@ -0,0 +1,17 @@
+return {
+
+  {"folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+		opts = {
+			transparent = true
+		},
+    config = function()
+			local color = "tokyonight"
+			vim.cmd.colorscheme(color)
+
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    end,}
+}
