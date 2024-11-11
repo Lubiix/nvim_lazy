a2d8640dddbab7af08225980742808ea1140f162
Lubiix <maxime.violi@gmail.com>
2024-11-11 09:04:30 +0100

    add remap & setup colors

--- lua/lubix/remap.lua
+++ lua/lubix/remap.lua
@@ -6,13 +6,23 @@ vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- In normal remap W & w by Z & z
+vim.api.nvim_set_keymap('n', 'Z', 'W', {noremap = true})
+vim.api.nvim_set_keymap('n', 'z', 'w', {noremap = true})
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+--Cursor stay in the middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Nav window 
+vim.keymap.set('n', '<leader>h', '<C-w>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "*", "*N", { noremap = true })
 
 -- Only for windows
-vim.api.nvim_set_keymap('n', '<C-q>', '<C-v>', { noremap = true })
+--vim.api.nvim_set_keymap('n', '<C-q>', '<C-v>', { noremap = true })
 
 vim.api.nvim_set_keymap("n", "<Esc>", ":noh<CR><Esc>", { noremap = true, silent = true })
 
