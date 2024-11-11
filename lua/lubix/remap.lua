067ade55e9e46a9e65cf9b363f5c60c6ca3c04c9
Lubiix <maxime.violi@gmail.com>
2024-11-11 03:43:47 +0100

    add undotree plus some remap

--- lua/lubix/remap.lua
+++ lua/lubix/remap.lua
@@ -9,3 +9,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+vim.api.nvim_set_keymap("n", "*", "*N", { noremap = true })
+
+-- Only for windows
+vim.api.nvim_set_keymap('n', '<C-q>', '<C-v>', { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<Esc>", ":noh<CR><Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>CF", "<cmd>e ~/.config/nvim/lua/lubix/plugins/lsp.lua<CR>");
+
