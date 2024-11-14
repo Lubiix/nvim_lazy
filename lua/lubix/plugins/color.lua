b91101950bd2aa856daba7eb54b4d8df0c7fb2ed
Lubiix <maxime.violi@gmail.com>
2024-11-14 11:56:28 +0100

    fix colors tmux & add comment column nvim

--- lua/lubix/plugins/color.lua
+++ lua/lubix/plugins/color.lua
@@ -13,5 +13,7 @@ return {
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    end,}
+			--vim.opt.colorcolumn = "80"
+			--vim.cmd [[highlight ColorColumn ctermbg=0 guibg=#555555]]
+		end,}
 }
